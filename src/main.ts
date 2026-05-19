d6f9d823f552aa0a498e3a4a2a287365b8ce29c6
Michael Bishop <michael@bishop.net.za>
2026-05-19 13:44:09 +0200

    Add extra versions

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ const DEFAULT_SETTINGS: BiblePluginSettings = {
   defaultTranslation: "niv",
 };
 
-const TRANSLATIONS = ["gnt", "niv"];
+const TRANSLATIONS = ["gnt", "niv", "ceb", "msg" ];
 const VIEW_TYPE = "bible-study-view";
 
 // ─── Database ─────────────────────────────────────────────────────────────────
